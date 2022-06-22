0a8129df2e0d20b1012ebf1e163dd84ee1bd89af
wrafanunes <wrsnunes@gmail.com>
2022-06-22 14:01:24 -0300

    corrige violação do ISP da classe ReferenceBook
    - Agora a classe ReferenceBook implementa IBook, que contém todas as informações de Book, exceto as propriedades CheckOutDurationInDays, Borrower e BorrowerDate e nem os métodos CheckIn, CHeckOut e GetDueDate, que são informações necessárias apenas para livros que podem sair da biblioteca por empréstimo

--- DemoLibrary/ReferenceBook.cs
+++ DemoLibrary/ReferenceBook.cs
@@ -6,33 +6,14 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-    /*Esta classe viola o Princípio de Segregação de Interface, já que um livro de referência de uma biblioteca
-     * não pode ser emprestado, sendo assim, não pode ter as propriedades CheckOutDurationInDays, Borrower e
-     BorrowerDate*/
-    public class ReferenceBook : ILibraryItem
+    /*Agora a classe ReferenceBook implementa IBook, que contém todas as informações de Book, exceto as propriedades
+    * CheckOutDurationInDays, Borrower e BorrowerDate e nem os métodos CheckIn, CHeckOut e GetDueDate, que são
+     informações necessárias apenas para livros que podem sair da biblioteca por empréstimo*/
+    public class ReferenceBook : IBook
     {
         public string LibraryId { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public int Pages { get; set; }
-        public int CheckOutDurationInDays { get; set; } = 0;
-        public string Borrower { get; set; }
-        public DateTime BorrowDate { get; set; }
-
-        //métodos não implementados, pois não são necessários
-        public void CheckIn ()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void CheckOut (string borrower)
-        {
-            throw new NotImplementedException();
-        }
-
-        public DateTime GetDueDate ()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
