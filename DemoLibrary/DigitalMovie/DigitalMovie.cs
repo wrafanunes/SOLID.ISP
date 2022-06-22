f3c60ee0b02b9da32b9b354db0fd93810f15baf6
wrafanunes <wrsnunes@gmail.com>
2022-06-22 16:47:51 -0300

    adiciona a classe DigitalMovie
    - adiciona uma nova classe chamada DigitalMovie, que implementa
      IBorrowableDVD
    - a classe DigitalMovie foi criada apenas a título de exemplo de como
      implementar uma interface bem definida e modularizada, em vez de
      utilizar a super interface ILibraryItem

---
+++ DemoLibrary/DigitalMovie/DigitalMovie.cs
@@ -0,0 +1,36 @@
+﻿using DemoLibrary.DVD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.DigitalMovie
+{
+    internal class DigitalMovie : IBorrowableDVD
+    {
+        public string LibraryId { get; set; }
+        public string Title { get; set; }
+        public int CheckOutDurationInDays { get; set; } = 14;
+        public string Borrower { get; set; }
+        public DateTime BorrowDate { get; set; }
+        public int RuntimeInMinutes { get; set; }
+        public List<string> Actors { get; set; }
+
+        public void CheckIn ()
+        {
+            Borrower = "";
+        }
+
+        public void CheckOut (string borrower)
+        {
+            Borrower = borrower;
+            BorrowDate = DateTime.Now;
+        }
+
+        public DateTime GetDueDate ()
+        {
+            return BorrowDate.AddDays(CheckOutDurationInDays);
+        }
+    }
+}
